5ab9c6c98bd23451b3f163dd950cce45899a6b4b
Boris Toet <boristoet@Device-van-Boris.local>
2017-06-02 12:06:52 +0200

    fix

--- lib/vimeo_me2/video/comment.rb
+++ lib/vimeo_me2/video/comment.rb
@@ -7,8 +7,9 @@ module VimeoMe2
       end
 
       def add_comment text
+        body = {}
         body['text'] = text
-        post('/comments', body:body)
+        post('/comments', body:body, code:201)
       end
 
       def view_comment comment_id
@@ -16,12 +17,13 @@ module VimeoMe2
       end
 
       def edit_comment comment_id, text
+        body = {}
         body['text'] = text
         patch("/comments/#{comment_id}", body:body)
       end
 
       def delete_comment comment_id
-        delete("/comments/#{comment_id}")
+        delete("/comments/#{comment_id}", code:204)
       end
 
     end
