e01880f0986194a6f696091757d22c809f203d30
bo-oz <boristoet80@gmail.com>
2018-12-13 23:09:22 +0100

    fix wrong hash

--- lib/vimeo_me2/user/upload.rb
+++ lib/vimeo_me2/user/upload.rb
@@ -49,7 +49,7 @@ module VimeoMe2
 
         # start the upload
         def start_upload
-          headers = {'Content-Type': 'application/offset+octet-stream'}
+          headers = {'Content-Type' => 'application/offset+octet-stream'}
           headers['Tus-Resumable'] = '1.0.0'
           headers['Upload-Offset'] = '0'
           @video.rewind
