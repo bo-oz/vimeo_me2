f2853e473bcb7a3d55a602d21566d7bc1d25a287
Chris Boertien <chris.boertien@gmail.com>
2021-03-17 10:55:22 -0300

    Update preset.rb
    Fix bug in preset path

--- lib/vimeo_me2/video/preset.rb
+++ lib/vimeo_me2/video/preset.rb
@@ -3,7 +3,7 @@ module VimeoMe2
     module Preset
 
       def add_preset preset_id
-        put("presets/#{preset_id}", code: 204)
+        put("/presets/#{preset_id}", code: 204)
       end
 
     end
